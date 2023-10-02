73c186af0b0c5530f3df7d165ca8cf5ce1c3e1b3
Muhammad Aldiansyah <muhaldiansyah2020@mhs.mdp.ac.id>
2023-10-02 15:21:06 +0700

    Upload Tugas

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Muhammad Aldiansyah' });
+});
+router.get('/profile', function(req, res){
+  res.render('profile')
+});
+module.exports = router;
